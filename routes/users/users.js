257663290db2e739442e31d4bfaf44810e1a3b95
Tina Davis <christina.davis@libertymutual.com>
2024-04-29 22:18:22 -0400

    users/authenticate POST endpoint working

--- routes/users/users.js
+++ routes/users/users.js
@@ -17,8 +17,27 @@ router.get('/', async (req, res) => {
         }   
 });
 
-router.post('/', async (req, res) => {
-    res.send("POST called");  
+router.post('/authenticate', async (req, res) => {
+    try {
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const loginCredentials = req.body;
+        const users = await collection.findOne({"user": loginCredentials.user});
+        if (!users) {
+            res.status(401);
+            res.send("Access Denied");
+        } else {
+            if (loginCredentials.password === users.password) {
+                res.status(200);
+                res.send("Authentication Successful");
+            } else {
+                res.status(401);
+                res.send("Incorrect Password");
+            }
+        }
+        } catch (err) {
+        res.status(500).json({ error: err.message });
+        }   
 });
 
 export default router;
