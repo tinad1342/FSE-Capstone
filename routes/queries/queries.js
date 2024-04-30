2006b40e90daca493de76c740fdaf1c572a19eed
Tina Davis <christina.davis@libertymutual.com>
2024-04-29 22:59:06 -0400

    queries POST & GET endpoints working

--- routes/queries/queries.js
+++ routes/queries/queries.js
@@ -1,12 +1,29 @@
 import express from "express";
+import fs from 'fs';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+    try {
+        const data = fs.readFileSync("queries.json", "utf8");
+        res.status(200);
+        res.send(data);
+    } catch (err) {
+        res.status(404);
+        res.send(err);
+    }  
 });
 
 router.post('/', async (req, res) => {
-    res.send("POST called");  
+    const savedQuery = req.body;
+    const data = JSON.stringify(savedQuery);
+    try {
+        fs.writeFileSync("queries.json", data);
+        res.status(200);
+        res.send("query array saved");
+    } catch (err) {
+        res.status(500);
+        res.send(err);
+    }
 });
 
 export default router;
